9e339b0e970371e0759aaefa0e618c53c96e5d92
R_Ashfell <luqmanulraiserhakim@gmail.com>
2021-10-09 18:07:21 +0800

    Basic css is done for the sketch area. Moving on to hover effect on each grid

--- myScript.js
+++ myScript.js
@@ -10,13 +10,15 @@ function gridSize (lineNum,perLineNum){
     let currentPerLine=1;
     for(;currentLine <= lineNum; currentLine++){
         const line = document.createElement('div');
-        line.textContent = 'Line One';
+        line.classList.add('line');
+        line.textContent = `Line ${currentLine}`;
         /* Create line div here*/
         container.appendChild(line)
         console.log(`currentLine: ${currentLine}`);
         for(;currentPerLine <= perLineNum; currentPerLine++){
             const perLine = document.createElement('div');
-            perLine.textContent = 'Box within Line';
+            perLine.classList.add('perLine');
+            perLine.textContent = `Box ${currentPerLine}`;
             /* Create grid within one line here*/
             line.appendChild(perLine);
             console.log(`currentPerLine: ${currentPerLine}`);
