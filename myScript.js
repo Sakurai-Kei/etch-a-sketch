9bd396e32aa1ec0304dd7248e86e055bcb5ae9b2
R_Ashfell <luqmanulraiserhakim@gmail.com>
2021-10-09 23:27:24 +0800

    Allowed the user to input a custom value from 1 to 64.Moving on to resetGridArea function.

--- myScript.js
+++ myScript.js
@@ -1,15 +1,42 @@
 const container = document.getElementById('container');
 let hovers;
+let buttonSelection;
+let size;
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        buttonSelection = button.id;
+        userInput(buttonSelection);
+    });
+});
+
+function userInput(buttonSelection) {
+    if(buttonSelection == 'size'){
+        size = prompt('Input Sketch Size. Please enter only one number up to 64');
+        if(parseInt(size) > 64){
+            alert('Input too large. A maximum value of 64 is allowed')
+        }
+
+        else if(parseInt(size) >= 1){
+            gridSize(size,size);
+        }
+
+        else{
+            alert('You have non integer value. Please re-enter valid value')
+        }
+    }
+}
 
 function gridSize (lineNum,perLineNum){
-    let currentLine=1;
-    let currentPerLine=1;
+    let currentLine = 1;
+    let currentPerLine = 1;
     for(;currentLine <= lineNum; currentLine++){
         const line = document.createElement('div');
         line.classList.add('line');
         /*line.textContent = `x`;*/
         /* Create line div here*/
-        container.appendChild(line)
+        container.appendChild(line);
         console.log(`currentLine: ${currentLine}`);
         for(;currentPerLine <= perLineNum; currentPerLine++){
             const perLine = document.createElement('div');
@@ -19,7 +46,7 @@ function gridSize (lineNum,perLineNum){
             line.appendChild(perLine);
             console.log(`currentPerLine: ${currentPerLine}`);
         }
-        currentPerLine=1;
+        currentPerLine = 1;
     }
    divEventListener();
 }
